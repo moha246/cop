8c23b190739a816a0f83ab1adba377436ea715dc
code-intensive <justtega97@gmail.com>
2023-02-02 20:38:39 +0100

    restructured folder structure

--- src/users/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from src.users.api.views import UserViewSet
-
-app_name = "users"
-
-user_router = SimpleRouter()
-user_router.register(r"", UserViewSet, basename="users")

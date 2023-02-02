097b021d5e314c3c8f9d0669554f023083d2382d
code-intensive <justtega97@gmail.com>
2023-02-02 20:38:05 +0100

    created BaseTimeStampModel

--- src/forums/models.py
+++ src/forums/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from src.core.mixins import TimeStampMixin
-from src.core.str_models import USER_MODEL
+from core.models import BaseTimeStampModel
+from core.str_models import USER_MODEL
 
 
-class Forum(TimeStampMixin):
-    name = models.CharField(_("Forum name"), max_length=65)
+class Forum(BaseTimeStampModel):
+    name = models.CharField(_("forum name"), max_length=65)
     members = models.ManyToManyField(USER_MODEL, related_name="forums")
     creator = models.CharField(_("Creator"), max_length=20)

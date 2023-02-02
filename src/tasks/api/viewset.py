5aa98d9ba815315aa3eb5d44e10baf70d7465939
code-intensive <justtega97@gmail.com>
2023-02-02 20:39:28 +0100

    added models, viewsets and serializers to tasks

---
+++ src/tasks/api/viewset.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+
+from tasks.api.serializers import TaskSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = TaskSerializer.Meta.model.objects.all()
--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from core.models import BaseTimeStampModel
+
+User = get_user_model()
+
+
+class Task(BaseTimeStampModel):
+    assigner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasks_assigned"
+    )
+    assignee = models.ManyToManyField(User, related_name="assigned_tasks")

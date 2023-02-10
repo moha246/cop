b0d42bbb41db9282f75a4703cb91ed9def0dd6cc
Dalhatu <musamohammaddal@gmail.com>
2023-02-10 12:17:47 +0100

    added posts

--- src/posts/api/viewsets.py
+++ src/posts/api/viewsets.py
@@ -1,10 +1,46 @@
 from rest_framework.viewsets import ModelViewSet
-
-from posts.api.serializers import PostSerializer
-from posts.models import Post
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.request import Request
+from posts.api.serializers import PostSerializer, CommentSerializer
+from posts.models import Post, Comment, LikedPost, LikedComment
 
 
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
+    model = Post
     serializer_class = PostSerializer
     lookup_url_kwarg = "post_id"
+
+    def get_queryset(self):
+        return self.model.objects.order_by()
+
+    def perform_create(self, serializer: PostSerializer) -> Post:
+        serializer.save(posted_by=self.request.user)
+
+    @action(detail=True, methods=("GET",))
+    def comments(self, request: Request, post_id: int) -> Response:
+        return Response(ComentSerializer(self.get_object().comments, many=True).data)
+
+    @action(
+        detail=True,
+        methods=("PATCH",),
+        url_path="comments/(?P<comment_id>[0-9]+)/add",
+    )
+    def comments_add(self, request: Request, post_id: int, comment_id: int) -> Response:
+        post = self.get_object()
+        comment = get_object_or_404(Comment, id=comment_id)
+        post.comments.add(comment)
+        return Response(CommentSerializer(post.comments, many=True).data)
+
+    @action(
+        detail=True,
+        methods=("DELETE",),
+        url_path="comments/(?P<comment_id>[0-9]+)/remove",
+    )
+    def comments_remove(
+        self, request: Request, post_id: int, comment_id: int
+    ) -> Response:
+        post = self.get_object()
+        comment = get_object_or_404(post.comments, id=comment_id)
+        post.comments.remove(comment)
+        return Response(CommentSerializer(post.comments, many=True).data)

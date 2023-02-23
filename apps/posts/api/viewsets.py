6a2f207157b8592dc93e69cf06a12b751b639096
Tega Ukavwe <justtega97@gmail.com>
2023-02-23 23:23:39 +0100

    finished unlike and like endpoints

--- apps/posts/api/viewsets.py
+++ apps/posts/api/viewsets.py
@@ -5,6 +5,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema
 
 from authentication.utils import has_admin_privileges
 from posts.api.serializers import PostSerializer, CommentSerializer
@@ -42,26 +43,28 @@ class CommentViewSet(ModelViewSet):
     def perform_create(self, serializer: CommentSerializer) -> Comment:
         serializer.save(commented_by=self.request.user, post_id=self.get_post_id())
 
+    @extend_schema(request=None, responses=None)
     @action(
         detail=True,
         methods=("POST",),
         url_path="like",
     )
     def like_comment(self, request: Request, post_id: int, comment_id: int) -> Response:
-        post = self.get_object()
-        comment = get_object_or_404(post.comments, pk=comment_id)
-        comment.likes.get_or_create(liked_by=request.user, comment=comment)
-        return Response(CommentSerializer(comment).data)
+        comments = get_object_or_404(Post, pk=post_id).comments
+        comment = get_object_or_404(comments, pk=comment_id)
+        comment.likes.get_or_create(liked_by=request.user)
+        return Response({"likes": comment.likes.count()})
 
+    @extend_schema(request=None, responses=None)
     @action(
         detail=True,
-        methods=("DELETE",),
+        methods=("POST",),
         url_path="unlike",
     )
     def unlike_comment(
         self, request: Request, post_id: int, comment_id: int
     ) -> Response:
-        post = self.get_object()
-        comment = get_object_or_404(post.comments, pk=comment_id)
+        comments = get_object_or_404(Post, pk=post_id).comments
+        comment = get_object_or_404(comments, pk=comment_id)
         get_object_or_404(comment.likes, liked_by=request.user).delete()
-        return Response(CommentSerializer(post.comments, many=True).data)
+        return Response({"likes": comment.likes.count()})

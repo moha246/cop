b772266c4518174785d6bc9349a377cc9b4e49e8
Tega Ukavwe <justtega97@gmail.com>
2023-02-23 21:57:23 +0100

    conditional slash appending to prefix_api_endpoint

--- config/routes/utils.py
+++ config/routes/utils.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 
 
-def prefix_api_endpoint(path: str, version: str = settings.API_VERSION) -> str:
+def prefix_api_endpoint(
+    path: str, version: str = settings.API_VERSION, append_slash: bool = True
+) -> str:
     """Prepends api version to the url path, the settings ``API_VERSION``
     variable is used by default.
     A different api version can optionally be passed to the function
     via the ``version`` argument"""
-    return f"api/{version}/{path}/"
+    return f"api/{version}/{path}/" if append_slash else f"api/{version}/{path}"

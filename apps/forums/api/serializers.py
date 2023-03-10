2f71497db02fecdbb5de1f24a5f917cda9f3b139
Tega Ukavwe <justtega97@gmail.com>
2023-03-10 11:12:06 +0100

    read only SlimSerializer used for forum serializer

--- apps/forums/api/serializers.py
+++ apps/forums/api/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework.serializers import ModelSerializer
 
 from forums import get_forum_model
-from users.api.serializers import UserSerializer
+from posts.api.serializers import SlimUserSerializer
 
 
 class ForumSerializer(ModelSerializer):
-    members = UserSerializer(many=True)
+    members = SlimUserSerializer(many=True, read_only=True)
 
     class Meta:
         model = get_forum_model()
         fields = "__all__"
-        read_only_fields = model.READ_ONLY_FIELDS + ["members"]
+        read_only_fields = model.READ_ONLY_FIELDS
